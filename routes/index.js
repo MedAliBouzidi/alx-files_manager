ee2c0a2f313ebe230f383c35c5d953721594fa74
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-06-15 14:54:50 +0200

    task 2

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+
+import AppController from '../controllers/AppController';
+import UserController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
+
+const router = Router();
+
+router.get('/status', AppController.getStatus);
+router.get('stats', AppController.getStats);
+router.post('/users', UserController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/diconnect', AuthController.getDisconnect);
+router.get('/users/me', UserController.getMe);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('files/:id/publish', FilesController.putPublish);
+router.put('files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getData);
+
+module.exports = router;
